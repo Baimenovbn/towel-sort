71d70ddf0ae678b4dee151871c089c3b86ea9ab9
baimenovbn <baibn@mail.ru>
2020-10-21 20:06:21 +0600

    feat: add towelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (!matrix || !matrix.length) return [];
+
+  let i = 0;
+  for (let row of matrix) {
+    if (i % 2 !== 0) {
+      row.sort((a, b) => a > b ? -1 : 1)
+    }
+    i++;
+  }
+  return matrix.map(row => row
+                            .join()
+                            .split(','))
+                                        .join()
+                                        .split(',')
+                                                  .map(str => parseInt(str));
+};
--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 const assert = require('assert');
-
 const towelSort = require('./src/index.js');
 
 describe('.towelSort, should perfrom \'towelSort\'', () => {
